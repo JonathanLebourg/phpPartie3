2a640bee12444f29180974f074b807cb39c9e8ae
JonathanLebourg <jonjonlebourg@gmail.com>
2018-11-21 11:26:47 +0100

    first commit

---
+++ exo6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>ExoPhp6 - Partie3</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet" /> 
+        <link href="style.css" rel="stylesheet"/>  
+    </head>
+    <body>
+        <div class="container">
+            <h1>ExoPhp6 - Partie3</h1>
+            <p>En allant de 20 à 0 avec un pas de 1, afficher le message <b>C'est presque bon</b>.</p>
+        </div>   
+        <div class="container"> 
+        <?php
+          for ($nb1=20; $nb1>=0; $nb1--) {
+           echo $nb1 . '  C\'est presque bon <br />';
+          }
+                
+          ?>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
